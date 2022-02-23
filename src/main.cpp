5bbf44c54c5335637fd941b5d27cd0f9a5d8d3c4
pedrohmx <pedrohx@yahoo.com.br>
2022-02-23 16:25:07 -0300

    better reporting and removed stderr report

--- src/main.cpp
+++ src/main.cpp
@@ -15,7 +15,7 @@ auto main(int argc, char *argv[]) -> int {
 
     for (fs::path p : args) {
         if (!fs::exists(p)) {
-            std::cerr << "Error: " << p << " does not exists.\n";
+            // std::cerr << "Error: " << p << " does not exists.\n";
             continue;
         } else if (fs::is_regular_file(p)) {
             targets.push_back(p);
@@ -28,10 +28,15 @@ auto main(int argc, char *argv[]) -> int {
         }
     }
 
+    //TODO: start csv
+    std::ofstream csv;
+    csv.open("results.csv");
+    csv << "FILE, MERGE_SORT(ms), SUPER_MERGE(ms),\n";
+
     int count = 0;
     for (auto p : targets){
         std::cout << "Target: " << p << '\n';
-        std::cerr << "Loading file (" << ++count << '/' << targets.size() << "): " << p << "\n";
+        std::cerr << "\33[2K\rLoading file (" << ++count << '/' << targets.size() << "): " << p;
 
         std::ifstream in;
         in.open(p);
@@ -42,32 +47,34 @@ auto main(int argc, char *argv[]) -> int {
         while (in >> aux_i)
             aux_vec.push_back(aux_i);
 
-        std::cerr << "\tAllocating memory\n";
         auto arr = new int[aux_vec.size()];
 
-        std::cerr << "\tCopying memory for mergesort.\n";
         std::copy(aux_vec.begin(),aux_vec.end(),arr);
         
-        std::cerr << "\tStarting mergesort\n";
         auto t1 = std::chrono::steady_clock::now();
         paa::merge_sort(arr,0,aux_vec.size());
         auto t2 = std::chrono::steady_clock::now();
-        auto span = std::chrono::duration_cast<std::chrono::duration<double,std::milli>>(t2 - t1);
-        std::cout << "\tmergesort: " << span.count() << " ms.\n";
+        auto span_ms = std::chrono::duration_cast<std::chrono::duration<double,std::milli>>(t2 - t1);
+        std::cout << "\tmergesort: " << span_ms.count() << " ms.\n";
 
-        std::cerr << "\tCopying memory for supermerge.\n";
         std::copy(aux_vec.begin(),aux_vec.end(),arr);
 
-        std::cerr << "\tStarting supermerge\n";
-        t1 = std::chrono::steady_clock::now();
+        auto t3 = std::chrono::steady_clock::now();
         paa::super_merge_sort(arr,0,aux_vec.size());
-        t2 = std::chrono::steady_clock::now();
-        span = std::chrono::duration_cast<std::chrono::duration<double,std::milli>>(t2 - t1);
-        std::cout << "\tsupermerge: " << span.count() << " ms.\n";
+        auto t4 = std::chrono::steady_clock::now();
+        auto span_sm = std::chrono::duration_cast<std::chrono::duration<double,std::milli>>(t4 - t3);
+        std::cout << "\tsupermerge: " << span_sm.count() << " ms.\n";
+
+        //TODO: add to csv file
+        csv << p << ',' << span_ms.count() << ',' << span_sm.count() << ",\n";
 
         delete[] arr;
         in.close();
     }
+    std::cerr << "\33[2K\rDone.\n";
+
+    //TODO: end csv
+    csv.close();
 
     return 0;
 }
