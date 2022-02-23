00e45b44542bcd4610ffae51a85d757c27f1a433
pedrohmx <pedrohx@yahoo.com.br>
2022-02-23 14:15:32 -0300

    Finished arg handling

--- src/main.cpp
+++ src/main.cpp
@@ -1,30 +1,72 @@
 #include <iostream>
+#include <fstream>
 #include <chrono>
+#include <string>
+#include <vector>
+#include <filesystem>
 
 #include "paa.hpp"
 
-int main(void) {
+namespace fs = std::filesystem;
 
-    int vet1[] = {9,8,7,6,5,4,3,2,1,0};
-    int vet2[] = {9,8,7,6,5,4,3,2,1,0};
-    
-    auto t1 = std::chrono::steady_clock::now();
-    paa::merge_sort(vet1, 0, 9);
-    paa::super_merge_sort(vet2, 0, 9);
-    auto t2 = std::chrono::steady_clock::now();
+auto main(int argc, char *argv[]) -> int {
+    std::vector<std::string> args(argv+1, argc + argv);
+    std::vector<fs::path> targets;
 
-    auto span = std::chrono::duration_cast<std::chrono::duration<double>>(t2 - t1);
+    for (fs::path p : args) {
+        if (!fs::exists(p)) {
+            std::cerr << "Error: " << p << " does not exists.\n";
+            continue;
+        } else if (fs::is_regular_file(p)) {
+            targets.push_back(p);
+        } else if (fs::is_directory(p)){
+            for (auto const& dir_entry : fs::directory_iterator{p}) {
+                if ( (dir_entry.is_regular_file()) && (dir_entry.path().extension() == ".txt") ){
+                    targets.push_back(dir_entry);
+                }
+            }
+        }
+    }
 
-    std::cout << "Took " << span.count() << " s.";
-    std::cout << std::endl;
+    for (auto p : targets){
+        std::cout << "Target: " << p << '\n';
+        std::cerr << "Loading file.\n";
 
-    for (int i = 0; i < 10; i++)
-        std::cout << vet1[i] << ' ';
-    std::cout << std::endl;
+        std::ifstream in;
+        in.open(p);
 
-    for (int i = 0; i < 10; i++)
-        std::cout << vet2[i] << ' ';
-    std::cout << std::endl;
+        std::vector<int> aux_vec;
+
+        int aux_i;
+        while (in >> aux_i)
+            aux_vec.push_back(aux_i);
+
+        std::cerr << "Allocating memory\n";
+        auto arr = new int[aux_vec.size()];
+
+        std::cerr << "Copying memory for mergesort.\n";
+        std::copy(aux_vec.begin(),aux_vec.end(),arr);
+        
+        std::cerr << "Starting mergesort\n";
+        auto t1 = std::chrono::steady_clock::now();
+        paa::merge_sort(arr,0,aux_vec.size());
+        auto t2 = std::chrono::steady_clock::now();
+        auto span = std::chrono::duration_cast<std::chrono::duration<double,std::milli>>(t2 - t1);
+        std::cout << "\tmergesort: " << span.count() << " ms.\n";
+
+        std::cerr << "Copying memory for supermerge.\n";
+        std::copy(aux_vec.begin(),aux_vec.end(),arr);
+
+        std::cerr << "Starting supermerge\n";
+        t1 = std::chrono::steady_clock::now();
+        paa::merge_sort(arr,0,aux_vec.size());
+        t2 = std::chrono::steady_clock::now();
+        span = std::chrono::duration_cast<std::chrono::duration<double,std::milli>>(t2 - t1);
+        std::cout << "\tsupermerge: " << span.count() << " ms.\n";
+
+        delete[] arr;
+        in.close();
+    }
 
     return 0;
 }
